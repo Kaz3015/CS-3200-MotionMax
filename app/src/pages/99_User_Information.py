ccf4475382e35be2e8d62d4349d15446d7bf91a3
Julien Motaharian <jmotaharian1@gmail.com>
2025-04-08 11:30:15 -0400

    added an api call to reference the list of user made workouts

--- app/src/pages/99_User_Information.py
+++ app/src/pages/99_User_Information.py
@@ -17,3 +17,9 @@ st.header('Client/User Information')
 # You can access the session state to make a more customized/personalized app experience
 st.write(f"### Hi, {st.session_state['first_name']}.")
 
+#LIST OF USER-MADE WORKOUTS
+st.write(f"## List of Current User-made Workouts")
+
+df = pd.DataFrame(requests.get(f'http://api:4000/c/workouts/by-client/{st.session_state["user_id"]}').json())
+st.dataframe(df)
+
