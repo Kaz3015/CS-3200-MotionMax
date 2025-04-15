806f391674d4a8a2c47d530fe59fd645827c90e4
Julien Motaharian <jmotaharian1@gmail.com>
2025-04-15 15:13:30 -0400

    create of the create a circuit screen

---
+++ app/src/pages/98_Create_Circuit.py
@@ -0,0 +1,13 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import streamlit as st
+import requests
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.express as px
+import pandas as pd
+
+#page configurations
+st.set_page_config(layout="wide")
+
