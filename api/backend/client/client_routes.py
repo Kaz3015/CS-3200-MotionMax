6cbf7649023801d84ff7b9279831d2979ac372bc
Julien Motaharian <jmotaharian1@gmail.com>
2025-04-17 01:23:18 -0400

    removed unnecessary routes

--- api/backend/client/client_routes.py
+++ api/backend/client/client_routes.py
@@ -98,132 +98,6 @@ def next_up_workout_exercise_information(user_id):
     response.status_code = 200
     return response
 
-@client.route('/equipment-based-search/', defaults={'equipment_string': ''}, methods=['GET'])
-@client.route('/equipment-based-search/<equipment_string>/', methods=['GET'])
-def make_equipment_based_search(equipment_string):
-    cursor = None
-    cursor = db.get_db().cursor()
-    
-    equipment_string = equipment_string.strip()
-    
-    if(equipment_string == ""):
-        query = '''
-            SELECT e.name, e.equipment_needed
-            FROM Exercise e
-        '''
-        cursor.execute(query)
-    else:
-        query = '''
-            SELECT e.name, e.equipment_needed
-            FROM Exercise e
-            WHERE e.equipment_needed LIKE CONCAT('%%', %s, '%%');
-        '''
-        cursor.execute(query, (equipment_string,))
-    
-    
-    theData = cursor.fetchall()
-    response = make_response(jsonify(theData))
-    response.status_code = 200
-    return response 
-
-
-@client.route('/difficulty-based-search/<beginner>/<intermediate>/<advanced>/', methods=['GET'])
-def make_difficulty_based_search(beginner, intermediate, advanced):
-    query = '''
-            SELECT e.name, e.difficulty
-            FROM Exercise e
-        '''
-    
-    prevDifficulty = False
-    
-    if beginner:
-        query += " WHERE e.difficulty = 'beginner'"
-        prevDifficulty = True
-    
-    if intermediate and prevDifficulty:
-        query += " OR e.difficulty = 'intermediate'"
-        prevDifficulty = True
-    elif intermediate:
-        query += " WHERE e.difficulty = 'intermediate'"
-
-    if advanced and prevDifficulty:
-        query += " OR e.difficulty = 'advanced'"
-        prevDifficulty = True
-    elif advanced:
-        query += " WHERE e.difficulty = 'advanced'"    
-    
-    query += ";"
-    
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    theData = cursor.fetchall()
-    response = make_response(jsonify(theData))
-    response.status_code = 200
-    return response 
-
-
-@client.route('/target-muscle-based-search/', defaults={'target_muscle_string': ''}, methods=['GET'])
-@client.route('/target-muscle-based-search/<target_muscle_string>/', methods=['GET'])
-def make_target_muscle_based_search(target_muscle_string):
-    cursor = None
-    cursor = db.get_db().cursor()
-    
-    equipment_string = target_muscle_string.strip()
-    
-    if(target_muscle_string == ""):
-        query = '''
-            SELECT e.name, e.target_muscle
-            FROM Exercise e
-        '''
-        cursor.execute(query)
-    else:
-        query = '''
-            SELECT e.name, e.target_muscle
-            FROM Exercise e
-            WHERE e.target_muscle LIKE CONCAT('%%', %s, '%%');
-        '''
-        cursor.execute(query, (target_muscle_string,))
-    
-    
-    theData = cursor.fetchall()
-    response = make_response(jsonify(theData))
-    response.status_code = 200
-    return response 
-
-
-
-
-@client.route('/exercise-type-based-search/', defaults={'exercise_type_string': ''}, methods=['GET'])
-@client.route('/exercise-type-based-search/<exercise_type_string>/', methods=['GET'])
-def make_exercise_type_based_search(exercise_type_string):
-    cursor = None
-    cursor = db.get_db().cursor()
-    
-    equipment_string = exercise_type_string.strip()
-    
-    if(exercise_type_string == ""):
-        query = '''
-            SELECT e.name, e.exercise_type
-            FROM Exercise e
-        '''
-        cursor.execute(query)
-    else:
-        query = '''
-            SELECT e.name, e.exercise_type
-            FROM Exercise e
-            WHERE e.exercise_type LIKE CONCAT('%%', %s, '%%');
-        '''
-        cursor.execute(query, (exercise_type_string,))
-    
-    
-    theData = cursor.fetchall()
-    response = make_response(jsonify(theData))
-    response.status_code = 200
-    return response 
-
-   
-   
-   
 @client.route('/workouts/next-scheduled/video-url/<user_id>/', methods=['GET'])
 def next_up_workout_exercise_video_url(user_id):
     query = f'''
@@ -277,26 +151,6 @@ def get_motivation_tip():
     response = make_response(jsonify(theData))
     response.status_code = 200
     return response
-
-@client.route('/health/today-meal-breakdown/<user_id>/', methods=['GET'])
-def todays_meal_breakdown(user_id):
-    query = f'''
-        SELECT fl.meal_type, SUM(fi.calories) AS `calories`, SUM(fi.protein) AS `protein`, SUM(fi.carbs) AS `carbs`, SUM(fi.fats) AS `fats`
-        FROM User u
-            JOIN FoodLog fl ON u.user_id = fl.user_id
-            JOIN FoodLog_FoodItem flfi on fl.food_log_id = flfi.food_log_id
-            JOIN FoodItem fi on flfi.food_item_id = fi.food_item_id
-        WHERE fl.date_logged = CURRENT_DATE AND u.user_id = {user_id}
-        GROUP BY fl.meal_type;
-    '''
-    
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    theData = cursor.fetchall()
-    response = make_response(jsonify(theData))
-    response.status_code = 200
-    return response
-
 
    
 @client.route('/food/food_intake_information/<meal_type>/<user_id>/', methods=['GET'])
@@ -406,21 +260,6 @@ def exercise_search(name, equipment, muscle_group, difficulty, exercise_type):
     rows = cursor.fetchall()
     cursor.close()
     return jsonify(rows), 200
-
-@client.route('/insert/default_user_circuit/<user_id>', methods=['GET'])
-def insert_default_user_circuit(user_id):
-    query = f'''
-            INSERT INTO Circuit(user_id, created_by, name, description, circuit_type, difficulty, target_muscle, equipment_needed)
-                VALUES
-                ({user_id}, {user_id}, 'No Name', 'No Description', 'strength', 'beginner', 'No Target Muscle', 'No Equipment Needed'),
-        ''' 
-    
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    theData = cursor.fetchall()
-    response = make_response(jsonify(theData))
-    response.status_code = 200
-    return response
 
 @client.route('/insert/circuit/<user_id>/<circuit_name>/<circuit_description>/<scheduled_date>/', methods=["POST"])
 def insert_circuit(user_id, circuit_name, circuit_description, scheduled_date):
