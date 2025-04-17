2d8b5891a691a9adad99780b55dda4000c679a13
Julien Motaharian <jmotaharian1@gmail.com>
2025-04-17 00:14:15 -0400

    changed the name of the circuits in 'List of My Workouts' from a button to a subheader; renamed 99_User_Information to User_Information; altered the page switches to reflect the name changes; added checks to prevent errors whenever a route returns an empty database; fixed video_url errors; added functionality for completing a set which includes updating the database and inserting into the workout logs; added arbiutary spacing; changed the header from 'Calorie Intake Over Time' to 'Macro Intake Over Time'

--- app/src/pages/User_Information.py
+++ app/src/pages/User_Information.py
@@ -11,34 +11,35 @@ import pandas as pd
 #page configurations
 st.set_page_config(layout="wide")
 
-
-
 r1c1, r1c2, r1c3 = st.columns([1, 3, 1], border=True)
 
 # Column involving the list of user-made workouts
 with r1c1:
     st.header("List of My Workouts")
     df = pd.DataFrame(requests.get(f'http://api:4000/c/workouts/by-client/{st.session_state["user_id"]}').json())
-
     for index, row in df.iterrows():
-        st.button(row['name'])
+        st.subheader(row['name'])
         
     st.write('')
     st.write('')
     
     if st.button('Create Circuit'):
-        st.switch_page('pages/98_Create_Circuit.py')
+        st.switch_page('pages/Create_Circuit.py')
     
 with r1c2:
     st.header("Breakdown of Today's Workout")
-    df = pd.DataFrame(requests.get(f'http://api:4000/c/workouts/currently-scheduled-exercises/{st.session_state["user_id"]}').json())
     
-    if df.empty:
-        st.write("No workout scheduled for today! Great job!")
+    next_exercise = pd.DataFrame(requests.get(f'http://api:4000/c/workouts/currently-scheduled/next-exercise/{st.session_state["user_id"]}').json())
+    
+    if next_exercise.empty:
+        st.write("Workout completed for today! Great job!")
     else:
+        associated_circuit_id = next_exercise.iloc[0]['circuit_id']
+        df = pd.DataFrame(requests.get(f'http://api:4000/c/workouts/currently-scheduled-exercises/{st.session_state["user_id"]}/{associated_circuit_id}/').json())
+        
         #Inner columns for the breakdown of today's workout
         i_c1, i_c2 = st.columns([1, 2], border=True)
-        
+            
         with i_c1:
             st.header("Muscle Group Info")
             st.write(df.iloc[0]['target_muscle'])
@@ -78,9 +79,10 @@ with r1c2:
         with i_c2:
             st.header("Next Up Exercise Information")
             df = pd.DataFrame(requests.get(f'http://api:4000/c/workouts/currently-scheduled/next-exercise/{st.session_state["user_id"]}').json())
-            
+
             st.write(f"Next Up: {df.iloc[0]['name']}")
             st.write(f"Target Muscle: {df.iloc[0]['target_muscle']}")
+            st.session_state['video_url'] = df.iloc[0]['video_url']
             
             if row['reps'] != 0:
                 st.write(f"Current Set: {row['reps']} reps with {row['rest_seconds']}s of rest with {row['weight']}lb")
@@ -94,17 +96,39 @@ with r1c2:
             st.write(df.iloc[0]['personal_notes'])
             
             st.header("Technique Video")
-            st.video(df.iloc[0]['video_url'])
             
+            st.video(st.session_state['video_url'])
             
-            st.button("Complete Set")
-        
+
+            if st.button("Complete Set"):
+                df = pd.DataFrame(requests.get(f"http://api:4000/c/workouts/currently-scheduled/next-exercise/{st.session_state['user_id']}").json())
+                exerciseset_id = df.iloc[0]['exerciseset_id']
+                exercise_id   = df.iloc[0]['exercise_id']
+                circuit_id    = df.iloc[0]['circuit_id']
+
+                df_set = requests.put(f"http://api:4000/c/update/complete_exercise_set/{exercise_id}/{exerciseset_id}/").json()
+
+                if df_set.get('status') == 'success':
+                    st.success("Set completed!")
+
+                    df = pd.DataFrame(requests.get(f"http://api:4000/c/workouts/currently-scheduled/next-exercise/{st.session_state['user_id']}").json())
+
+                    if df.empty:
+                        df = requests.post(f"http://api:4000/c/insert/workout_log/{st.session_state['user_id']}/{circuit_id}/").json()
+
+                        if df.get("status") == "success":
+                            st.success("Entire circuit complete—workout logged!")
+                        else:
+                            st.error(f"Failed to log workout: {df.get('message')}")
+                else:
+                    st.error(f"Error completing set: {df_set.get('message')}")
     
 with r1c3:
     st.header("Status of Workout")
     df = pd.DataFrame(requests.get(f'http://api:4000/c/workouts/next-scheduled/video-url/{st.session_state["user_id"]}').json())
+    next_exercise = pd.DataFrame(requests.get(f'http://api:4000/c/workouts/currently-scheduled/next-exercise/{st.session_state["user_id"]}').json())
     
-    if df.empty:
+    if df.empty or next_exercise.empty:
         st.write(":green[Congrats! Your workout is completed!]")
     else:
         st.write(":red[You haven't completed your workout for today! Remember to complete it!]")
@@ -112,8 +136,12 @@ with r1c3:
     st.header("Motivation")
     df = pd.DataFrame(requests.get(f'http://api:4000/c/motivation/tip/').json())
     
-    st.write(df.iloc[0]['text'])
-    
+    if df.empty:
+        st.write("No motivation tip available right now.")
+    else:
+        st.write(df.iloc[0]['text'])
+
+
 
 r2c1, r2c2, r2c3 = st.columns([1, 3, 1], border=True)
 
@@ -192,8 +220,28 @@ with r2c2:
             st.write(f"Carbohydrates: {row['carbs']}g")
             st.write(f"Fats: {row['fats']}g")
             
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    
+    if st.button("Add Food Item"):
+        st.switch_page('pages/Add_Food.py')
+            
 with r2c3:
-    st.header("Calorie Intake Over Time")
+    st.header(f"Macro Intake Over Time")
     
     num_days = st.slider("Chart Time Frame", 1, 30, 6)
     nutrients_option = st.selectbox("Type of Nutrients", ("Calories", "Protein", "Carbohydrates", "Fats"), placeholder="Calories")
@@ -213,4 +261,4 @@ with r2c3:
     st.header("General Health Tips")
     df = pd.DataFrame(requests.get(f'http://api:4000/c/health/tip/').json())
     
-    st.write(df.iloc[0]['text'])    
+    st.write(df.iloc[0]['text'])
